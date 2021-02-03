d526cda453d537f99a3a773c0f3f105970df600c
Christopher Watkins <chrisbwatkins@gmail.com>
2021-02-03 14:19:24 -0700

    made a button for better understand the data struture

--- script.js
+++ script.js
@@ -57,32 +57,22 @@ random.on('click',function(){
 })
 
 $("body").append(random);
-var playbutton = $("<button>");
-var player = $("<audio autoplay>")
-var sources = $('<source>')
-playbutton.on('click',function(){
-   
-    if(i < 0){
-        i = 0;
-    }
-    DZ.api('search?q=metallica',function(response){
-        console.log(response);
-        console.log((response.data[i].duration / 60));
-        console.log(response.data[i].title);
-        console.log(response.data[i].artist.name);
-        sources.attr("src",response.data[0].preview);
-        sources.attr("type","audio/mp3"); 
-         
-    });
 
-   
+var iframebutton = $("<button>").text("iframeplayer")
+var i = 0;
+
+iframebutton.on('click',function(){
+
+    i++
+    var iframe = $("<iframe>").attr("src","https://www.deezer.com/plugins/player?format=square&autoplay=false&playlist=false&width=300&height=300&color=EF5466&layout=&size=medium&type=artist&id="+ i + "&app_id=0").attr("width","300").attr("height","300").attr("scrolling","no").attr("frameborder","0").attr("allowTransparency","true");
+    
+    $("body").append(iframe);
   
+})
 
-player.append(sources)
-});
-$("body").append(player);
+$("body").append(iframebutton);
 
-$("body").append(playbutton)
+
 
 
 
